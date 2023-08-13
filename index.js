4de94b163327da3c861f8504bbeeb62960b4b689
Thomas Werthman <tommygun3151224@gmail.com>
2023-08-13 11:14:46 -0400

    Refactored code to make more readable, added gravity

--- index.js
+++ index.js
@@ -4,20 +4,16 @@ const c = canvas . getContext('2d')
 canvas.width = 64 * 16
 canvas.height = 64 * 9
 
-c.fillStyle = 'white'
-c.fillRect(0,0, canvas.width, canvas.height)
+const player = new Player()
 
-c.fillStyle = 'red'
-c.fillRect(100, 100, 100, 100)
-
-let y = 100
+//let bottom = y + 100
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'white'
     c.fillRect(0,0, canvas.width, canvas.height)
-    c.fillStyle = 'red'
-    c.fillRect(100, y, 100, 100)
-    y++
+
+    player.draw()
+    player.update()   
 }
 
-animate()
+animate() 
